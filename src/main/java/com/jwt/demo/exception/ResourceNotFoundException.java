9d7abca810b53c4224adebfb26d5ec3edcde8f8a
kilanooru17 <165049709+kilanooru17@users.noreply.github.com>
2026-05-27 15:45:15 +0530

    Create ResourceNotFoundException.java

---
+++ src/main/java/com/jwt/demo/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.jwt.demo.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
